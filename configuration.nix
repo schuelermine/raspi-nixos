cff96e75a926e5a5446520085178483ad9d1a98d
Anselm Schüler <mail@anselmschueler.com>
2022-07-15 15:23:02 +0200

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }: {
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+  };
+  networking = {
+    hostName = "raspberry-pi";
+    networkmanager.enable = true;
+    wireless = {
+      enable = true;
+      networks.jackocktagon.psk = "gorgonzola";
+      interfaces = [ "wlan0" ];
+    };
+  };
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+  users = {
+    mutableUsers = false;
+    users.anselmschueler = {
+      isNormalUser = true;
+      description = "Anselm Schüler";
+      hashedPassword =
+        "$y$j9T$MggYbRbZoK.1WEW96K1el0$hcbo3SFFwDbvI5BPV4GdW2jWTAR1fMd56ssqeSgb8r5";
+      extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = [
+        "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADVkv3VPlmMB5+251kdlVae/RnH6VLHIK756Y83/a2jMpMZH1D9Tqwl8H9WSkZUMpZDXf15zKvrmJid+MxAkWfSSwCCu58btaFAVa6iIK9Zm6bbWEk7FeF3gd2n5cvW2dki2PovBc2yFUpE2ao+90BhEtfwouU+rrVRru1zcyEkmvO5FA== mail@anselmschueler.com"
+      ];
+    };
+  };
+  programs.fish.enable = true;
+  services = {
+    openssh = {
+      enable = true;
+      passwordAuthentication = false;
+    };
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+    };
+    xserver = {
+      enable = true;
+      displayManager.lightdm.enable = true;
+      desktopManager.xfcs.enable = true;
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+  outputs = { self, nixos-hardware, nixpkgs }: {
+    nixosModules.main-config = import ./configuration.nix;
+    nixosConfigurations.raspberry-pi = nixpkgs.lib.nixosSystem {
+      system = "aarch65";
+      modules = [
+        self.nixosModules.main-config
+        nixos-hardware.nixosModules.raspberry-pi-4
+      ];
+    };
+    nixosConfigurations.raspberry-pi-sdImage = nixpkgs.lib.nixosSystem {
+      system = "aarch65";
+      modules = [
+        self.nixosModules.main-config
+        nixos-hardware.nixosModules.raspberry-pi-4
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image.nix"
+        ./bootloader.nix
+      ];
+    };
+  };
+}
